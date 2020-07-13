a210d95adf2bae0f1de15df7280711ec7ff792e4
Khalid Williams <khalidwill27@gmail.com>
2020-07-13 07:38:59 -0600

    Remove knex

--- index.js
+++ index.js
@@ -4,28 +4,62 @@ const port = process.env.PORT || 3001;
 
 const app = express();
 app.use(bodyParser.json());
+app.locals = [
+        {
+          id: 1,
+          title: "First Post",
+          author: "Johnny B",
+          content: "Do the monkey",
+          score: 0,
+          comments: [
+            {
+              content: "First!",
+              id: 11,
+              parentPost: 1
+            },
+            {
+              content: "Second!",
+              id: 12,
+              parentPost: 1
+            },
+            {
+              content: "Last :(!",
+              id: 13,
+              parentPost: 1
+            }
+          ]
+        },
+        {
+          id: 2,
+          title: "I think it hailed yesterday",
+          author: "Some Fella",
+          content: "It was wild stuff",
+          score: 0,
+          comments: [
+            {
+              content: "I saw it too!",
+              id: 154,
+              parentPost: 2
+            },
+            {
+              content: "I don't believe in hail",
+              id: 12345232,
+              parentPost: 2
+            }
+          ]
+        }
 
-const environment = process.env.NODE_ENV || 'development';
-const configuration = require('./knexfile.js')[environment];
-const db = require('knex')(configuration);
+];
+
+
 
 const versionedBase = '/api/v1';
 
-app.get(`${versionedBase}/posts`, async (request, response) => {
+app.get(`${versionedBase}/posts`, (request, response) => {
   try {
-    let allPosts = await db('posts').select();
-    allPosts = allPosts.map(async (post) => {
-      const comments = await db('comments').select('id', 'content').where({
-        parentPost: post.id 
-      });
-      post.comments = comments;
-      return post
-    })
-    const postsWithComments = await Promise.all(allPosts);
-    response.status(200).json(postsWithComments);
+    response.status(200).json({posts: app.locals});
   } catch (error) {
-    console.error(error);
-    response.status(500).json({error});
+    response.status(500).json(error);
   }
 });
 
