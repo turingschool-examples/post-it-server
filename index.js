eef407e0e45575476f64619594f2903323d2cded
Khalid Williams <khalidwill27@gmail.com>
2020-07-13 08:40:01 -0600

    Add posts

--- index.js
+++ index.js
@@ -48,8 +48,8 @@ app.locals = [
             }
           ]
         }
+  ];
 
-];
 
 
 
@@ -61,6 +61,40 @@ app.get(`${versionedBase}/posts`, (request, response) => {
   } catch (error) {
     response.status(500).json(error);
   }
+});
+
+app.post(`${versionedBase}/posts`, (request, response) => {
+  try {
+    const { title, author, content } = request.body;
+    const reqProps = {title, author, content};
+    for ( prop in reqProps ) {
+      if (!reqProps[prop]) throw new Error(`Prop ${prop} is not defined`);
+    }
+    const newPost = { title, author, content };
+    newPost.id = Date.now();
+    newPost.score = 0;
+    newPost.comments = [];
+    app.locals.push(newPost);
+    response.status(201).json(newPost);
+  } catch (error) {
+    response.status(422).json({error: error.message});
+  }
+});
+
+app.post(`${versionedBase}/posts/:postId/comments`, (request, response) => {
+  try {
+    const { postId } = request.params;
+    const parentPost = app.locals.find(post => post.id == postId);
+    if (!parentPost) throw new Error (`No post with id ${postId} found`);
+    
+    const { content } = request.body;
+    if ( !content ) throw new Error(`No content property found on request`);
+    parentPost.comments.push({id:Date.now(), parentPost:postId, content:content});
+    response.status(201).json(parentPost);
+  } catch(error) {
+    response.status(422).json({error: error.message});
+  }
+
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
